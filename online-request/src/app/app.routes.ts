bbc744bf29d7a0b863af438c8b25de74820360f2
nawinp <hwnd.19@gmail.com>
2025-10-06 16:16:38 +0700

    Login Page

--- online-request/src/app/app.routes.ts
+++ online-request/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Login } from './Compoment/login/login';
+import { Register } from './Compoment/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: Login },
+    { path: 'register', component: Register }
+];
